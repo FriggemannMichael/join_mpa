244776d3e5c5059fc2cc7ef88c78123c6e5bd9ef
Marc-Andre Buck <marcbuck94@gmail.com>
2025-11-06 13:06:19 +0100

    fix(utils): reconnect showAlert in showBoardAlert to restore board notifications

--- js/board/utils.js
+++ js/board/utils.js
@@ -2,6 +2,7 @@ import { auth, db } from "../common/firebase.js";
 import { icons } from "../common/svg-template.js";
 import { ref, update } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 import {unmountAddTaskValidation} from "../validation/validation-addTask.js"
+import {showAlert} from "../common/alertService.js"
 
 
 /**
